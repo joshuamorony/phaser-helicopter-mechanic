b60f86d89d71511486bd43a678bf10af8393bef7
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-02-10 09:38:45 +1030

    initial commit

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,4 +1,5 @@
-import ExampleObject from 'objects/ExampleObject';
+import Helicopter from 'objects/Helicopter';
+import MovingWalls from 'objects/MovingWalls';
 
 class Main extends Phaser.State {
 
@@ -10,12 +11,42 @@ class Main extends Phaser.State {
 		//Set the games background colour
 		this.game.stage.backgroundColor = '#cecece';
 
-		//Example of including an object
-		//let exampleObject = new ExampleObject(this.game);
+		this.helicopter = new Helicopter(this.game);
+		this.helicopter.spawn();
+
+		this.walls = new MovingWalls(this.game);
+
+		this.addControls();
+		this.addTimers();
 	}
 
 	update() {
-		
+
+		this.game.physics.arcade.overlap(this.helicopter.sprite, this.walls.spriteGroup, this.gameOver, null, this);
+
+		// Check if out of bounds
+		if(this.helicopter.isOutOfBounds()){
+			this.gameOver();
+		}
+
+		// Check if  helicopter is rising
+		if(this.helicopter.isRising){
+			this.helicopter.increaseVerticalVelocity();
+		}
+
+	}
+
+	addControls(){
+		this.game.input.onDown.add(this.helicopter.setRising, this.helicopter);
+		this.game.input.onUp.add(this.helicopter.setFalling, this.helicopter);
+	}
+
+	addTimers(){
+		this.game.time.events.loop(2000, this.walls.spawn, this.walls);
+	}
+
+	gameOver(){
+		this.game.state.restart();
 	}
 
 }
